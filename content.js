2d973668aede24b961ed180349f8f2d26985b08b
HirayamaSachi <hirayamas0305@gmail.com>
2023-01-11 23:47:17 +0900

    add:クリック時にイベント発火

--- content.js
+++ content.js
@@ -1,18 +1,20 @@
-// コメントを非表示にする
-var diffProgressiveContainer = document.getElementsByClassName('js-diff-progressive-container').item(0);
-var allChangedFileContainer = diffProgressiveContainer.querySelectorAll('[data-targets="diff-file-filter.diffEntries"]');
-for (ChangedFileContainer of allChangedFileContainer) {
-    ChangedFileContainer.classList.remove('show-inline-notes');
-}
+(function () {
+    // コメントを非表示にする
+    var diffProgressiveContainer = document.getElementsByClassName('js-diff-progressive-container').item(0);
+    var allChangedFileContainer = diffProgressiveContainer.querySelectorAll('[data-targets="diff-file-filter.diffEntries"]');
+    for (ChangedFileContainer of allChangedFileContainer) {
+        ChangedFileContainer.classList.remove('show-inline-notes');
+    }
 
-// Show commentsのチェックを外す
-var dropDownMenu = document.getElementsByClassName('show-more-popover');
-for (let i = 0; i < dropDownMenu.length; i++) {
-    var toggleComments=dropDownMenu[i].querySelectorAll('[aria-checked="true"]');
-    if(toggleComments.length==0){
-        continue;
+    // Show commentsのチェックを外す
+    var dropDownMenu = document.getElementsByClassName('show-more-popover');
+    for (let i = 0; i < dropDownMenu.length; i++) {
+        var toggleComments = dropDownMenu[i].querySelectorAll('[aria-checked="true"]');
+        if (toggleComments.length == 0) {
+            continue;
+        }
+        for (const toggleComment of toggleComments) {
+            toggleComment.ariaChecked = false;
+        }
     }
-    for (const toggleComment of toggleComments) {
-        toggleComment.ariaChecked=false;
-    }
-}
+})();
